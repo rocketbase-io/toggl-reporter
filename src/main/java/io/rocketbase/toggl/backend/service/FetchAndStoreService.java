c86c0b9da6a0b36fd6687a57193318242d731a09
Marten Prieß <marten@rocketbase.io>
2017-03-14 17:03:05 +0100

    splitted pull in case of huge range in order to avoid bad-request... max one year is allowed as differ between from and to

--- src/main/java/io/rocketbase/toggl/backend/service/FetchAndStoreService.java
+++ src/main/java/io/rocketbase/toggl/backend/service/FetchAndStoreService.java
@@ -7,7 +7,9 @@ import io.rocketbase.toggl.backend.model.DateTimeEntryGroupModel;
 import io.rocketbase.toggl.backend.repository.DateTimeEntryGroupRepository;
 import lombok.SneakyThrows;
 import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -32,17 +34,33 @@ public class FetchAndStoreService {
     @SneakyThrows
     public void fetchBetween(Date from, Date to) {
 
+
         DateTime fetchedDate = DateTime.now();
-        List<TimeEntry> timeEntryList = FetchAllDetailed.getAll(togglService.getTogglReportApi()
-                .detailed()
-                .since(from)
-                .until(to));
+        List<TimeEntry> timeEntryList = new ArrayList<>();
+
+        LocalDate fromAsLocalDate = LocalDate.fromDateFields(from);
+        LocalDate toAsLocalDate = LocalDate.fromDateFields(to);
+
+        if (Days.daysBetween(fromAsLocalDate, toAsLocalDate)
+                .getDays() > 100) {
+            LocalDate startFrom = LocalDate.fromDateFields(from);
+            do {
+                LocalDate toSub = startFrom.plusDays(99);
+                if (toAsLocalDate.isBefore(toSub)) {
+                    toSub = toAsLocalDate;
+                }
+                timeEntryList.addAll(fetch(startFrom, toSub));
+                startFrom = startFrom.plusDays(100);
+            } while (startFrom.isBefore(toAsLocalDate));
+        } else {
+            timeEntryList.addAll(fetch(fromAsLocalDate, toAsLocalDate));
+        }
 
         dateTimeEntryGroupRepository.deleteByWorkspaceIdAndDateBetween(togglService.getWorkspaceId(),
                 LocalDate.fromDateFields(from)
                         .minusDays(1)
                         .toDate(),
-                LocalDate.fromDateFields(to)
+                toAsLocalDate
                         .plusDays(1)
                         .toDate());
 
@@ -64,5 +82,14 @@ public class FetchAndStoreService {
         });
         dateTimeEntryGroupRepository.save(newEntities);
 
+    }
+
+    private List<TimeEntry> fetch(LocalDate from, LocalDate to) {
+        return FetchAllDetailed.getAll(togglService.getTogglReportApi()
+                .detailed()
+                .since(from.toDateTime(LocalTime.MIDNIGHT)
+                        .toDate())
+                .until(to.toDateTime(new LocalTime(23, 59, 59, 999))
+                        .toDate()));
     }
 }
